ac2fe5cfe48dddd95771ce2306e85598ef7b5d4d
unknown <madhavraninga3@gmail.com>
2025-05-12 23:37:38 +0530

    m

--- selection-sort/1.cpp
+++ selection-sort/1.cpp
@@ -1,97 +1,50 @@
-<<<<<<< HEAD
 #include<iostream>
 using namespace std;
 
 int main()
 {
     int size;
-    
-    cout << "Enter the Size of Array = ";
+    cout << "Enter the size of Array = ";
     cin >> size;
-    cout << "---------------------------------" << endl;
-    int box[size];
 
+    int box[size];
     for (int i = 0; i < size; i++)
     {
         cout << "box[" << i << "] = ";
         cin >> box[i];
     }
-    cout << "---------------------------------" << endl;
-
-    cout << "Array Value = ";
+    cout << "-------------------------------------" << endl;
+    cout << "Before Sorting = ";
     for (int i = 0; i < size; i++)
     {
-      cout << box[i] << " ";
+        cout << box[i] << " ";
     }
     cout << endl;
-    int min, temp;
-    for (int i = 0; i < size-1; i++)
+    cout << "-------------------------------------" << endl;
+
+    int temp;
+    for (int i = 0; i < size - 1; i++)
     {
-        min = i;
-        for (int j = i; j < size ; j++)
+        int minIndex = i;
+        for (int j = i; j < size; j++)
         {
-            if (box[j] < box[min])
+            if (box[j] < box[minIndex])
             {
-                min = j;
+                minIndex = j;
             }
         }
         temp = box[i];
-        box[i] = box[min];
-        box[min] = temp;
+        box[i] = box[minIndex];
+        box[minIndex] = temp;
+        
     }
-    cout << "Array Value = ";
+    cout << "-------------------------------------" << endl;
+    cout << "After Sorting = ";
     for (int i = 0; i < size; i++)
     {
-      cout << box[i] << " ";
+        cout << box[i] << " ";
     }
     cout << endl;
-=======
-#include<iostream>
-using namespace std;
+    cout << "-------------------------------------" << endl;
 
-int main()
-{
-    int size;
-    
-    cout << "Enter the Size of Array = ";
-    cin >> size;
-    cout << "---------------------------------" << endl;
-    int box[size];
-
-    for (int i = 0; i < size; i++)
-    {
-        cout << "box[" << i << "] = ";
-        cin >> box[i];
-    }
-    cout << "---------------------------------" << endl;
-
-    cout << "Array Value = ";
-    for (int i = 0; i < size; i++)
-    {
-      cout << box[i] << " ";
-    }
-    cout << endl;
-    int min, temp;
-    for (int i = 0; i < size-1; i++)
-    {
-        min = i;
-        for (int j = i; j < size ; j++)
-        {
-            if (box[j] < box[min])
-            {
-                min = j;
-            }
-        }
-        temp = box[i];
-        box[i] = box[min];
-        box[min] = temp;
-    }
-    cout << "Array Value = ";
-    for (int i = 0; i < size; i++)
-    {
-      cout << box[i] << " ";
-    }
-    cout << endl;
-    
->>>>>>> a3f527eab3cc605e13ca064c8b6aa96a325589cf
 }
